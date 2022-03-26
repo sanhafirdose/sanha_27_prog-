16fd80d0c715b570c4a5d91125f23c335e361733
SANHA-FIRDOSEFI <101562045+sanhafirdose@users.noreply.github.com>
2022-03-26 03:56:32 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main()
+ {
+   float unit,total,charge;
+   char name[20];
+
+   //accept custoner name 
+   printf("enter your name:");
+   gets(name);
+
+   //accept no of units consumed
+   printf("enter the units:");
+   scanf("%f",&unit);
+   charge=0;
+   if(unit>=1&&unit<=200)
+   {
+     charge=0.8*unit;
+   }
+   else if("unit>=201&&unit<=300")
+   {
+     charge=0.8*(200)+0.9*(unit-200);
+   }
+   else if (unit>300)
+   {
+     charge=0.8*(200)+0.9*(100)+1*(unit-300);
+   }
+   total=charge+100;
+   if(total>400)
+   {
+     total=total+(0.15*total);
+   }
+
+
+   printf("\n\nELECTRICITY BILL\n");
+  
+  
+   printf("\nName:%s\n",name);
+   printf("no of units:%2f\n",unit);
+   printf("total bill amount Rs.%.2f\n",total);
+   printf("-------\n");
+   return 0;
+ }
+
+
+
+
+
+
+
+
+  
